f333d946336bc89dce3f26064d656c8d37b74219
tlkahn <joshgy@gmail.com>
2024-05-25 21:33:47 +0800

    Refactor BottomPlayer component layout and conditional rendering. Update BottomPlayer component layout for better conditional rendering

--- src/components/BottomPlayer.js
+++ src/components/BottomPlayer.js
@@ -74,47 +74,51 @@ const BottomPlayer = () => {
   );
 
   return (
-    <div className="fixed bottom-0 left-0 right-0 bg-gray-800 py-8 px-8 flex flex-col items-center justify-center">
-      <div className="w-1/2 flex justify-between">
-        <button
-          className="text-white flex flex-col items-center"
-          onClick={togglePlayPause}
-        >
-          <i className={isPlaying ? 'fa fa-pause' : 'fa fa-play'}></i>
-          <span className="hidden sm:inline w-16 text-center">
-            {isPlaying ? 'Pause' : 'Play'}
-          </span>
-        </button>
-        <button
-          className="text-white flex flex-col items-center"
-          onClick={toggleRecord}
-        >
-          <i className={isRecording ? 'fa fa-circle' : 'fa fa-circle-o'}></i>
-          <span className="hidden sm:inline w-16 text-center">
-            {isRecording ? 'Stop' : 'Record'}
-          </span>
-        </button>
-        <button
-          className={`flex flex-col items-center ${
-            isPlaying ? 'text-gray-400' : 'text-white'
-          }`}
-          onClick={isPlaying ? undefined : handleChoose}
-        >
-          <i className="fa fa-ellipsis-h"></i>
-          <span className="hidden sm:inline w-16 text-center">Choose</span>
-        </button>
-      </div>
-      {isPopupVisible && <BgmPopup />}
-      {currentBgm && (
-        <div className="fixed bottom-0 left-0 right-0 mt-20 mx-8 text-center text-white">
-          {isPlaying ? (
-            <span>Now Playing: {currentBgm.name}</span>
-          ) : (
-            <span>{currentBgm.name}</span>
+    <>
+      <div className="fixed bottom-0 left-0 right-0 bg-gray-800 py-8 px-8 flex flex-col items-center justify-center">
+        <div className="w-1/2 flex justify-between">
+          {currentBgm && (
+            <button
+              className="text-white flex flex-col items-center"
+              onClick={togglePlayPause}
+            >
+              <i className={isPlaying ? 'fa fa-pause' : 'fa fa-play'}></i>
+              <span className="hidden sm:inline w-16 text-center">
+                {isPlaying ? 'Pause' : 'Play'}
+              </span>
+            </button>
           )}
+          <button
+            className="text-white flex flex-col items-center"
+            onClick={toggleRecord}
+          >
+            <i className={isRecording ? 'fa fa-circle' : 'fa fa-circle-o'}></i>
+            <span className="hidden sm:inline w-16 text-center">
+              {isRecording ? 'Stop' : 'Record'}
+            </span>
+          </button>
+          <button
+            className={`flex flex-col items-center ${
+              isPlaying ? 'text-gray-400' : 'text-white'
+            }`}
+            onClick={isPlaying ? undefined : handleChoose}
+          >
+            <i className="fa fa-ellipsis-h"></i>
+            <span className="hidden sm:inline w-16 text-center">Choose</span>
+          </button>
         </div>
-      )}
-    </div>
+        {currentBgm && (
+          <div className="fixed w-full bottom-0 left-0 right-0 mt-20 text-center text-white bg-gray-700 bg-opacity-50">
+            {isPlaying ? (
+              <span>Now Playing: {currentBgm.name}</span>
+            ) : (
+              <span>{currentBgm.name}</span>
+            )}
+          </div>
+        )}
+        {isPopupVisible && <BgmPopup />}
+      </div>
+    </>
   );
 };
 
