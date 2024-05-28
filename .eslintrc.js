02806284f37044815b74e99bc9b034ab00030410
tlkahn <joshgy@gmail.com>
2024-05-28 08:26:39 +0800

    Add ESLint configuration file. Configure ESLint rules for the project

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'eslint:recommended'],
+  plugins: ['react'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+};
