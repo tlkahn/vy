79e5ad82177c85c55adb2fbbef836a8657ce6618
tlkahn <joshgy@gmail.com>
2024-05-28 13:51:27 +0800

    Refactor LiveRooms layout to adjust grid columns. Update LiveRooms room item display for better alignment and styling

--- src/components/LiveRooms.js
+++ src/components/LiveRooms.js
@@ -39,11 +39,11 @@ const LiveRooms = () => {
         <div className="main container mx-auto px-4 py-8 flex flex-col md:flex-row h-full space-y-4 md:space-y-0">
           <SideMenu />
           <div id="main-content" className="w-full md:w-3/4 lg:w-6/7 pt-4">
-            <ul className="grid grid-cols-3 sm:grid-cols-5 lg:grid-cols-7 gap-4">
+            <ul className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 gap-4">
               {rooms.map((room) => (
                 <li
                   key={room.id}
-                  className="flex flex-col items-center cursor-pointer hover:bg-gray-600 p-2 rounded-lg relative overflow-hidden"
+                  className="cursor-pointer hover:bg-gray-600 p-2 rounded-lg relative overflow-hidden"
                   onClick={() => handleRoomClick(room.id)}
                   style={{
                     backgroundImage: `url('http://localhost:3000/images/room1.jpeg')`,
@@ -69,12 +69,17 @@ const LiveRooms = () => {
                       backgroundColor: 'rgba(0, 0, 0, 0.3)', // Adjust the opacity (0.5) to make it more or less dimmed
                     }}
                   ></div>
-                  <div className="z-10 relative">
-                    <div className="text-md text-center font-bold">
+                  <div className="z-10 relative aspect-w-1 aspect-h-1">
+                    <div className="text-md text-center font-bold flex flex-col justify-center items-center w-full">
                       {room.channel}
                     </div>
-                    <div className="text-sm">
-                      <i className="fa fa-clock-o"></i> {activeFor(room)}
+                    <div className="text-sm flex items-center justify-center mt-8 w-full">
+                      <span className="inline-flex items-center justify-center w-6">
+                        <i className="fa fa-clock-o"></i>
+                      </span>
+                      <span className="inline-flex items-center justify-center w-16">
+                        {activeFor(room)}
+                      </span>
                     </div>
                   </div>
                 </li>
