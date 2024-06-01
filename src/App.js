1af9bf6a61c4c183c3d11dffba8d0fe7e857b8a1
tlkahn <joshgy@gmail.com>
2024-06-01 13:06:26 +0800

    Lazy load Chatroom component with Suspense in App routing. Optimize component loading for better performance

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // src/App.js
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Suspense, lazy } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
 import Signup from './components/Signup';
@@ -11,7 +11,8 @@ import { UserAuthContextProvider } from './context/UserAuthContext';
 import LiveRoom from './components/LiveRoom';
 import ProtectedRoute from './components/ProtectedRoute';
 import { QuestionHistoryProvider } from './context/QuestionHistoryContext';
-import Chatroom from './components/Chatroom';
+
+const Chatroom = lazy(() => import('./components/Chatroom'));
 
 function App() {
   const [, setMessage] = useState('');
@@ -26,19 +27,21 @@ function App() {
     <BrowserRouter>
       <UserAuthContextProvider>
         <QuestionHistoryProvider>
-          <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/chat" element={<Chatroom />} />
-            {/* Below are protected domains */}
-            <Route path="*" element={<ProtectedRoute />}>
-              <Route path="liverooms" element={<LiveRooms />} />
-              <Route path="home" element={<MainComponent />} />
-              <Route path="liveroom/:roomId" element={<LiveRoom />} />
-              <Route path="sequence/:id" element={<SequenceComponent />} />
-              <Route path="category/:id" element={<CategoryComponent />} />
-            </Route>
-          </Routes>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/chat" element={<Chatroom />} />
+              {/* Below are protected domains */}
+              <Route path="*" element={<ProtectedRoute />}>
+                <Route path="liverooms" element={<LiveRooms />} />
+                <Route path="home" element={<MainComponent />} />
+                <Route path="liveroom/:roomId" element={<LiveRoom />} />
+                <Route path="sequence/:id" element={<SequenceComponent />} />
+                <Route path="category/:id" element={<CategoryComponent />} />
+              </Route>
+            </Routes>
+          </Suspense>
         </QuestionHistoryProvider>
       </UserAuthContextProvider>
     </BrowserRouter>
