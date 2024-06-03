7419ef4b7ebd1e9c8e1647c6aaf66331652b2071
tlkahn <joshgy@gmail.com>
2024-06-03 10:30:33 +0800

    Update user authentication logic and improve user data handling

--- src/App.js
+++ src/App.js
@@ -19,14 +19,6 @@ function App() {
     log.setLevel('info');
   }
 
-  const [, setMessage] = useState('');
-
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/todos/1') // Example endpoint from JSONPlaceholder
-      .then((response) => response.json())
-      .then((data) => setMessage(data.title));
-  }, []);
-
   return (
     <BrowserRouter>
       <UserAuthContextProvider>
