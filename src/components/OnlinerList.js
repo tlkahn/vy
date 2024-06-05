92344ccd2e98247cb920a7a0c6ce382b78721d36
tlkahn <joshgy@gmail.com>
2024-06-05 09:39:16 +0800

    Add getIcon function to determine user device/browser icon in OnlinerList. Update icon rendering to use FontAwesome

--- src/components/OnlinerList.js
+++ src/components/OnlinerList.js
@@ -2,6 +2,35 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Jdenticon from 'react-jdenticon';
 import ProfileModal from './ProfileModal';
+import platform from 'platform';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faAndroid,
+  faApple,
+  faChrome,
+  faFirefoxBrowser,
+  faInternetExplorer,
+} from '@fortawesome/free-brands-svg-icons';
+import { faGlobe } from '@fortawesome/free-solid-svg-icons';
+
+const getIcon = () => {
+  const os = platform.os.family.toLowerCase();
+  const browser = platform.name.toLowerCase();
+
+  if (os.includes('ios') || os.includes('iphone') || os.includes('ipad')) {
+    return faApple;
+  } else if (os.includes('android')) {
+    return faAndroid;
+  } else if (browser.includes('chrome')) {
+    return faChrome;
+  } else if (browser.includes('firefox')) {
+    return faFirefoxBrowser;
+  } else if (browser.includes('internet explorer')) {
+    return faInternetExplorer;
+  } else {
+    return faGlobe; // Default icon for unknown cases
+  }
+};
 
 function OnlinerList({ onliners }) {
   const [profileUserId, setProfileUserId] = useState(null);
@@ -25,15 +54,7 @@ function OnlinerList({ onliners }) {
               >
                 <Jdenticon size="48" value={onliner.email} />
                 <span className="text-sm">{onliner.email}</span>
-                <i
-                  className={`text-sm ${
-                    onliner.device === 'iphone'
-                      ? 'fa fa-apple'
-                      : onliner.device === 'android'
-                      ? 'fa fa-android'
-                      : 'fa fa-globe'
-                  }`}
-                ></i>
+                <FontAwesomeIcon icon={getIcon()} />
               </li>
             );
           })}
