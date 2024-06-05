31346a18e360e35dea5b464f34eeadaaa7d48d3a
tlkahn <joshgy@gmail.com>
2024-06-05 10:30:29 +0800

    Update ProtectedRoute to display a loading message while verifying user

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import { useUserAuth } from '../context/UserAuthContext';
-import Login from './Login';
 import log from 'loglevel';
 
 const verifyUser = (user) => {
@@ -13,7 +12,15 @@ const ProtectedRoute = () => {
   const { user } = useUserAuth();
   log.info(`user from userUserAuth hook:`, { user });
 
-  return verifyUser(user) ? <Outlet /> : <Login />;
+  return verifyUser(user) ? (
+    <Outlet />
+  ) : (
+    <div className="fixed inset-0 flex items-center justify-center bg-gray-700">
+      <div className="bg-white p-4 rounded shadow-md w-40 text-center">
+        <p>Loading...</p>
+      </div>
+    </div>
+  );
 };
 
 export default ProtectedRoute;
